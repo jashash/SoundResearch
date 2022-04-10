ab4a65c3eb264474c45ae9700baf5ec69e595f59
jashash <73246088+jashash@users.noreply.github.com>
2022-04-10 14:59:34 -0700

    Stole drew's research from week1

--- main.py
+++ main.py
@@ -1,21 +1,146 @@
+#Completely stole all this from drew
+
+#from scipy.io.wavfile import write
+import soundfile as sf
+import multiprocessing as mp
 import sounddevice as sd
-from scipy.io.wavfile import write
-# import wavio as wv
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Sampling frequency
-freq = 44100
 
-# Recording duration
-duration = 5
+def rec_audio(frames, which_device, storage_array, channel):
+    print("in device" + str(which_device) + "\n")
+    data = sd.rec(frames, device=which_device, channels=channel, samplerate=44100)
+    sd.wait()
+    storage_array.put(data)
 
-# Start recorder with the given values of
-# duration and sample frequency
-recording = sd.rec(int(duration * freq),
-                   samplerate=freq, channels=2)
+if __name__ == '__main__':
+    #mp.set_start_method('fork')
+    recordings = mp.Queue()
+    seconds = 10
+    sampleRate = 44100
+    handheldProcess = mp.Process(target=rec_audio, args=(int(seconds * sampleRate), 1, recordings, 2))
+    webcamProcess = mp.Process(target=rec_audio, args=(int(seconds * sampleRate), 1, recordings, 1))
 
-# Record audio for the given number of seconds
-sd.wait()
+    handheldProcess.start()
+    # webcamProcess.start()
+    print("Recording audio...")
+    handheldProcess.join(15)
+    handheldRecArray = recordings.get()
+    # sf.write('TwoChannelStereo.wav', handheldRecArray, sampleRate)  # Save as WAV file
+    # webcamProcess.join(1)
+    # webcamRecArray = recordings.get()
+    # sf.write('OneChannelMono.wav', webcamRecArray, sampleRate)  # Save as WAV file
+    # print("Finished Recording.")
+    #
+    # #sf.read reads the same number of channels as the array
+    # MonoSourceStereoOutput = sf.read('OneChannelMono.wav', always_2d=True)
+    # StereoSourceStereoOutput = sf.read('TwoChannelStereo.wav')
+    #
+    # MonoSourceStereoOutput = np.array(MonoSourceStereoOutput)
+    # StereoSourceStereoOutput = np.array(StereoSourceStereoOutput)
+    # print(MonoSourceStereoOutput)
+    # print(StereoSourceStereoOutput)
+    #
+    # plt.title("Channel 1 Stereo")
+    # plt.plot(StereoSourceStereoOutput[:0], color="red")
+    #
+    # plt.show()
+    #
+    # plt.title("Channel 2 Stereo")
+    # plt.plot(StereoSourceStereoOutput[:1], color="red")
+    # #
+    # plt.show()
+    #
+    # plt.title("Channel 1 Mono")
+    # plt.plot(MonoSourceStereoOutput[:0], color="red")
+    # #
+    # plt.show()
+    #
+    # plt.title("Channel 2 Mono")
+    # plt.plot(MonoSourceStereoOutput[:1], color="red")
+    # #
+    # plt.show()
 
-# This will convert the NumPy array to an audio
-# file with the given sampling frequency
-write("recording0.wav", freq, recording)
+
+    # np.set_printoptions(precision=3)
+    # print("Original Size: " + str(handheldRecArray.size))
+    # print(str(handheldRecArray.ndim))
+    # print (handheldRecArray)
+    #
+    # newArray = np.array(handheldRecArray)
+    # x = seconds
+    # y = handheldRecArray
+    #
+    noise = np.random.normal(0, .001, handheldRecArray.shape)
+    new_signal = handheldRecArray + noise
+    noise2 = np.random.normal(0, .0002, handheldRecArray.shape)
+    new_signal2 = handheldRecArray + noise2
+    noise3 = np.random.normal(0, .0003, handheldRecArray.shape)
+    new_signal3 = handheldRecArray + noise3
+    #sf.write('OneChannelMono.wav', webcamProcess, sampleRate)  # Save as WAV file
+
+    #how to print
+    #when doing multi channel stuff
+    plt.title("Channel 1 No noise")
+    plt.plot(handheldRecArray[:, 0], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 2 No noise")
+    plt.plot(handheldRecArray[:, 1], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 1 With Noise")
+    plt.plot(new_signal[:, 0], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 2 With noise")
+    plt.plot(new_signal[:, 1], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 1 With Noise")
+    plt.plot(new_signal2[:, 0], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 2 With noise")
+    plt.plot(new_signal2[:, 1], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 1 With Noise")
+    plt.plot(new_signal3[:, 0], color="red")
+    #
+    plt.show()
+
+    plt.title("Channel 2 With noise")
+    plt.plot(new_signal3[:, 1], color="red")
+    #
+    plt.show()
+
+    #ii = 0
+    #while handheldRecArray[:, 0] <= 0.01:
+        #ii += 1
+
+    # shiftFactor = int(newArray.size/5)
+    # newArray.resize(newArray.size + shiftFactor)
+    # newArray = np.roll(newArray, shiftFactor)
+    #
+    # print("Adjusted Size: " + str(newArray.size))
+    #
+    # plt.title("Shifted Recording")
+    # plt.plot(newArray, color="red")
+    # #
+    # plt.show()
+
+
+
+# handheldMicRec = sd.rec(int(seconds * sampleRate), device=1)
+# webcamMicRec = sd.rec(int(seconds * sampleRate), device=2)
+# print("Recording audio...")
+# sd.wait()  # Wait until recording is finished
+# print("Finished Recording.")
